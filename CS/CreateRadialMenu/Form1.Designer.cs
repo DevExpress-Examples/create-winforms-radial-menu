eab5daa3de0a5ef5108b99d9034d88b3ddc79c6c
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/CreateRadialMenu/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace CreateRadialMenu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.barManager1 = new DevExpress.XtraBars.BarManager(this.components);
+            this.barDockControlTop = new DevExpress.XtraBars.BarDockControl();
+            this.barDockControlBottom = new DevExpress.XtraBars.BarDockControl();
+            this.barDockControlLeft = new DevExpress.XtraBars.BarDockControl();
+            this.barDockControlRight = new DevExpress.XtraBars.BarDockControl();
+            this.bar1 = new DevExpress.XtraBars.Bar();
+            this.bar2 = new DevExpress.XtraBars.Bar();
+            this.bar3 = new DevExpress.XtraBars.Bar();
+            this.barButtonItem1 = new DevExpress.XtraBars.BarButtonItem();
+            this.imageCollection1 = new DevExpress.Utils.ImageCollection(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.barManager1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageCollection1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // barManager1
+            // 
+            this.barManager1.Bars.AddRange(new DevExpress.XtraBars.Bar[] {
+            this.bar1,
+            this.bar2,
+            this.bar3});
+            this.barManager1.DockControls.Add(this.barDockControlTop);
+            this.barManager1.DockControls.Add(this.barDockControlBottom);
+            this.barManager1.DockControls.Add(this.barDockControlLeft);
+            this.barManager1.DockControls.Add(this.barDockControlRight);
+            this.barManager1.Form = this;
+            this.barManager1.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
+            this.barButtonItem1});
+            this.barManager1.MainMenu = this.bar2;
+            this.barManager1.MaxItemId = 1;
+            this.barManager1.StatusBar = this.bar3;
+            // 
+            // barDockControlTop
+            // 
+            this.barDockControlTop.CausesValidation = false;
+            this.barDockControlTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.barDockControlTop.Location = new System.Drawing.Point(0, 0);
+            this.barDockControlTop.Size = new System.Drawing.Size(777, 51);
+            // 
+            // barDockControlBottom
+            // 
+            this.barDockControlBottom.CausesValidation = false;
+            this.barDockControlBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.barDockControlBottom.Location = new System.Drawing.Point(0, 433);
+            this.barDockControlBottom.Size = new System.Drawing.Size(777, 23);
+            // 
+            // barDockControlLeft
+            // 
+            this.barDockControlLeft.CausesValidation = false;
+            this.barDockControlLeft.Dock = System.Windows.Forms.DockStyle.Left;
+            this.barDockControlLeft.Location = new System.Drawing.Point(0, 51);
+            this.barDockControlLeft.Size = new System.Drawing.Size(0, 382);
+            // 
+            // barDockControlRight
+            // 
+            this.barDockControlRight.CausesValidation = false;
+            this.barDockControlRight.Dock = System.Windows.Forms.DockStyle.Right;
+            this.barDockControlRight.Location = new System.Drawing.Point(777, 51);
+            this.barDockControlRight.Size = new System.Drawing.Size(0, 382);
+            // 
+            // bar1
+            // 
+            this.bar1.BarName = "Tools";
+            this.bar1.DockCol = 0;
+            this.bar1.DockStyle = DevExpress.XtraBars.BarDockStyle.Top;
+            this.bar1.Text = "Tools";
+            // 
+            // bar2
+            // 
+            this.bar2.BarName = "Main menu";
+            this.bar2.DockCol = 0;
+            this.bar2.DockStyle = DevExpress.XtraBars.BarDockStyle.Top;
+            this.bar2.LinksPersistInfo.AddRange(new DevExpress.XtraBars.LinkPersistInfo[] {
+            new DevExpress.XtraBars.LinkPersistInfo(this.barButtonItem1)});
+            this.bar2.OptionsBar.MultiLine = true;
+            this.bar2.OptionsBar.UseWholeRow = true;
+            this.bar2.Text = "Main menu";
+            // 
+            // bar3
+            // 
+            this.bar3.BarName = "Status bar";
+            this.bar3.CanDockStyle = DevExpress.XtraBars.BarCanDockStyle.Bottom;
+            this.bar3.DockCol = 0;
+            this.bar3.DockStyle = DevExpress.XtraBars.BarDockStyle.Bottom;
+            this.bar3.OptionsBar.AllowQuickCustomization = false;
+            this.bar3.OptionsBar.DrawDragBorder = false;
+            this.bar3.OptionsBar.UseWholeRow = true;
+            this.bar3.Text = "Status bar";
+            // 
+            // barButtonItem1
+            // 
+            this.barButtonItem1.Caption = "Show";
+            this.barButtonItem1.Id = 0;
+            this.barButtonItem1.Name = "barButtonItem1";
+            this.barButtonItem1.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.barButtonItem1_ItemClick);
+            // 
+            // imageCollection1
+            // 
+            this.imageCollection1.ImageStream = ((DevExpress.Utils.ImageCollectionStreamer)(resources.GetObject("imageCollection1.ImageStream")));
+            this.imageCollection1.Images.SetKeyName(0, "copy16x16.png");
+            this.imageCollection1.Images.SetKeyName(1, "cut16x16.png");
+            this.imageCollection1.Images.SetKeyName(2, "delete16x16.png");
+            this.imageCollection1.Images.SetKeyName(3, "paste16x16.png");
+            this.imageCollection1.Images.SetKeyName(4, "bold16x16.png");
+            this.imageCollection1.Images.SetKeyName(5, "italic16x16.png");
+            this.imageCollection1.Images.SetKeyName(6, "underLine16x16.png");
+            this.imageCollection1.Images.SetKeyName(7, "find16x16.png");
+            this.imageCollection1.Images.SetKeyName(8, "undo16x16.png");
+            this.imageCollection1.Images.SetKeyName(9, "redo16x16.png");
+            this.imageCollection1.Images.SetKeyName(10, "repeat16x16.png");
+            this.imageCollection1.Images.SetKeyName(11, "replace16x16.png");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(777, 456);
+            this.Controls.Add(this.barDockControlLeft);
+            this.Controls.Add(this.barDockControlRight);
+            this.Controls.Add(this.barDockControlBottom);
+            this.Controls.Add(this.barDockControlTop);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.barManager1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageCollection1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraBars.BarManager barManager1;
+        private DevExpress.XtraBars.Bar bar1;
+        private DevExpress.XtraBars.Bar bar2;
+        private DevExpress.XtraBars.BarButtonItem barButtonItem1;
+        private DevExpress.XtraBars.Bar bar3;
+        private DevExpress.XtraBars.BarDockControl barDockControlTop;
+        private DevExpress.XtraBars.BarDockControl barDockControlBottom;
+        private DevExpress.XtraBars.BarDockControl barDockControlLeft;
+        private DevExpress.XtraBars.BarDockControl barDockControlRight;
+        private DevExpress.Utils.ImageCollection imageCollection1;
+    }
+}
+
---
+++ CS/CreateRadialMenu/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.XtraBars.Ribbon;
+using DevExpress.XtraBars;
+
+namespace CreateRadialMenu {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void barButtonItem1_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e) {
+            // Create and display Radial Menu
+            barManager1.ItemClick += new ItemClickEventHandler(barManager1_ItemClick);
+            RadialMenu menu = new RadialMenu();
+            menu.Manager = barManager1;
+            menu.AddItems(CreateBarItems());
+
+            Point pt = this.Location;
+            pt.Offset(this.Width / 2, this.Height / 2);
+            menu.ShowPopup(pt);
+        }
+
+        BarItem[] CreateBarItems() {
+            // Create bar items to display in Radial Menu
+            barManager1.Images = imageCollection1;
+
+            BarItem barButtonItem0 = new BarButtonItem(barManager1, "Copy", 0);
+            BarItem barButtonItem1 = new BarButtonItem(barManager1, "Cut", 1);
+            BarItem barButtonItem2 = new BarButtonItem(barManager1, "Delete", 2);
+            BarItem barButtonItem3 = new BarButtonItem(barManager1, "Paste", 3);
+
+            // Sub-menu with 3 check buttons
+            BarSubItem subMenu = new BarSubItem(barManager1, "Format");
+            BarCheckItem barCheckItem4 = new BarCheckItem(barManager1, false) {
+                ImageIndex = 4,
+                Caption = "Bold"
+            };
+            BarCheckItem barCheckItem5 = new BarCheckItem(barManager1, true) {
+                ImageIndex = 5,
+                Caption = "Italic"
+            };
+            BarCheckItem barCheckItem6 = new BarCheckItem(barManager1, false) {
+                ImageIndex = 6,
+                Caption = "Underline"
+            };
+            BarItem[] subMenuItems = new BarItem[] { barCheckItem4, barCheckItem5, barCheckItem6 };
+            subMenu.AddItems(subMenuItems);
+
+            BarItem barButtonItem7 = new BarButtonItem(barManager1, "Find", 7);
+            BarItem barButtonItem8 = new BarButtonItem(barManager1, "Undo", 8);
+            BarItem barButtonItem9 = new BarButtonItem(barManager1, "Redo", 9);
+
+            return new BarItem[] {barButtonItem0, barButtonItem1, barButtonItem2, barButtonItem3, 
+                subMenu, barButtonItem7, barButtonItem8, barButtonItem9};
+        }
+
+        
+
+        void barManager1_ItemClick(object sender, ItemClickEventArgs e) {
+            //...
+        }
+    }
+}
