ec38ada68bb7aead09101459d51e9a7992f18197
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to: Create and show a Radial Menu
+
+
+<p>This example shows how to create a Radial Menu displaying a sub-menu, buttons and check buttons.</p>
+
+<br/>
+
+
---
+++ VB/CreateRadialMenu/Form1.Designer.vb
@@ -0,0 +1,167 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Namespace CreateRadialMenu
+	Partial Public Class Form1
+		''' <summary>
+		''' Required designer variable.
+		''' </summary>
+		Private components As System.ComponentModel.IContainer = Nothing
+
+		''' <summary>
+		''' Clean up any resources being used.
+		''' </summary>
+		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+			If disposing AndAlso (components IsNot Nothing) Then
+				components.Dispose()
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
+
+		#Region "Windows Form Designer generated code"
+
+		''' <summary>
+		''' Required method for Designer support - do not modify
+		''' the contents of this method with the code editor.
+		''' </summary>
+		Private Sub InitializeComponent()
+			Me.components = New System.ComponentModel.Container()
+			Dim resources As New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+			Me.barManager1 = New DevExpress.XtraBars.BarManager(Me.components)
+			Me.barDockControlTop = New DevExpress.XtraBars.BarDockControl()
+			Me.barDockControlBottom = New DevExpress.XtraBars.BarDockControl()
+			Me.barDockControlLeft = New DevExpress.XtraBars.BarDockControl()
+			Me.barDockControlRight = New DevExpress.XtraBars.BarDockControl()
+			Me.bar1 = New DevExpress.XtraBars.Bar()
+			Me.bar2 = New DevExpress.XtraBars.Bar()
+			Me.bar3 = New DevExpress.XtraBars.Bar()
+			Me.barButtonItem1 = New DevExpress.XtraBars.BarButtonItem()
+			Me.imageCollection1 = New DevExpress.Utils.ImageCollection(Me.components)
+			CType(Me.barManager1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me.imageCollection1, System.ComponentModel.ISupportInitialize).BeginInit()
+			Me.SuspendLayout()
+			' 
+			' barManager1
+			' 
+			Me.barManager1.Bars.AddRange(New DevExpress.XtraBars.Bar() { Me.bar1, Me.bar2, Me.bar3})
+			Me.barManager1.DockControls.Add(Me.barDockControlTop)
+			Me.barManager1.DockControls.Add(Me.barDockControlBottom)
+			Me.barManager1.DockControls.Add(Me.barDockControlLeft)
+			Me.barManager1.DockControls.Add(Me.barDockControlRight)
+			Me.barManager1.Form = Me
+			Me.barManager1.Items.AddRange(New DevExpress.XtraBars.BarItem() { Me.barButtonItem1})
+			Me.barManager1.MainMenu = Me.bar2
+			Me.barManager1.MaxItemId = 1
+			Me.barManager1.StatusBar = Me.bar3
+			' 
+			' barDockControlTop
+			' 
+			Me.barDockControlTop.CausesValidation = False
+			Me.barDockControlTop.Dock = System.Windows.Forms.DockStyle.Top
+			Me.barDockControlTop.Location = New System.Drawing.Point(0, 0)
+			Me.barDockControlTop.Size = New System.Drawing.Size(777, 51)
+			' 
+			' barDockControlBottom
+			' 
+			Me.barDockControlBottom.CausesValidation = False
+			Me.barDockControlBottom.Dock = System.Windows.Forms.DockStyle.Bottom
+			Me.barDockControlBottom.Location = New System.Drawing.Point(0, 433)
+			Me.barDockControlBottom.Size = New System.Drawing.Size(777, 23)
+			' 
+			' barDockControlLeft
+			' 
+			Me.barDockControlLeft.CausesValidation = False
+			Me.barDockControlLeft.Dock = System.Windows.Forms.DockStyle.Left
+			Me.barDockControlLeft.Location = New System.Drawing.Point(0, 51)
+			Me.barDockControlLeft.Size = New System.Drawing.Size(0, 382)
+			' 
+			' barDockControlRight
+			' 
+			Me.barDockControlRight.CausesValidation = False
+			Me.barDockControlRight.Dock = System.Windows.Forms.DockStyle.Right
+			Me.barDockControlRight.Location = New System.Drawing.Point(777, 51)
+			Me.barDockControlRight.Size = New System.Drawing.Size(0, 382)
+			' 
+			' bar1
+			' 
+			Me.bar1.BarName = "Tools"
+			Me.bar1.DockCol = 0
+			Me.bar1.DockStyle = DevExpress.XtraBars.BarDockStyle.Top
+			Me.bar1.Text = "Tools"
+			' 
+			' bar2
+			' 
+			Me.bar2.BarName = "Main menu"
+			Me.bar2.DockCol = 0
+			Me.bar2.DockStyle = DevExpress.XtraBars.BarDockStyle.Top
+			Me.bar2.LinksPersistInfo.AddRange(New DevExpress.XtraBars.LinkPersistInfo() { New DevExpress.XtraBars.LinkPersistInfo(Me.barButtonItem1)})
+			Me.bar2.OptionsBar.MultiLine = True
+			Me.bar2.OptionsBar.UseWholeRow = True
+			Me.bar2.Text = "Main menu"
+			' 
+			' bar3
+			' 
+			Me.bar3.BarName = "Status bar"
+			Me.bar3.CanDockStyle = DevExpress.XtraBars.BarCanDockStyle.Bottom
+			Me.bar3.DockCol = 0
+			Me.bar3.DockStyle = DevExpress.XtraBars.BarDockStyle.Bottom
+			Me.bar3.OptionsBar.AllowQuickCustomization = False
+			Me.bar3.OptionsBar.DrawDragBorder = False
+			Me.bar3.OptionsBar.UseWholeRow = True
+			Me.bar3.Text = "Status bar"
+			' 
+			' barButtonItem1
+			' 
+			Me.barButtonItem1.Caption = "Show"
+			Me.barButtonItem1.Id = 0
+			Me.barButtonItem1.Name = "barButtonItem1"
+'			Me.barButtonItem1.ItemClick += New DevExpress.XtraBars.ItemClickEventHandler(Me.barButtonItem1_ItemClick);
+			' 
+			' imageCollection1
+			' 
+			Me.imageCollection1.ImageStream = (CType(resources.GetObject("imageCollection1.ImageStream"), DevExpress.Utils.ImageCollectionStreamer))
+			Me.imageCollection1.Images.SetKeyName(0, "copy16x16.png")
+			Me.imageCollection1.Images.SetKeyName(1, "cut16x16.png")
+			Me.imageCollection1.Images.SetKeyName(2, "delete16x16.png")
+			Me.imageCollection1.Images.SetKeyName(3, "paste16x16.png")
+			Me.imageCollection1.Images.SetKeyName(4, "bold16x16.png")
+			Me.imageCollection1.Images.SetKeyName(5, "italic16x16.png")
+			Me.imageCollection1.Images.SetKeyName(6, "underLine16x16.png")
+			Me.imageCollection1.Images.SetKeyName(7, "find16x16.png")
+			Me.imageCollection1.Images.SetKeyName(8, "undo16x16.png")
+			Me.imageCollection1.Images.SetKeyName(9, "redo16x16.png")
+			Me.imageCollection1.Images.SetKeyName(10, "repeat16x16.png")
+			Me.imageCollection1.Images.SetKeyName(11, "replace16x16.png")
+			' 
+			' Form1
+			' 
+			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+			Me.ClientSize = New System.Drawing.Size(777, 456)
+			Me.Controls.Add(Me.barDockControlLeft)
+			Me.Controls.Add(Me.barDockControlRight)
+			Me.Controls.Add(Me.barDockControlBottom)
+			Me.Controls.Add(Me.barDockControlTop)
+			Me.Name = "Form1"
+			Me.Text = "Form1"
+			CType(Me.barManager1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.imageCollection1, System.ComponentModel.ISupportInitialize).EndInit()
+			Me.ResumeLayout(False)
+
+		End Sub
+
+		#End Region
+
+		Private barManager1 As DevExpress.XtraBars.BarManager
+		Private bar1 As DevExpress.XtraBars.Bar
+		Private bar2 As DevExpress.XtraBars.Bar
+		Private WithEvents barButtonItem1 As DevExpress.XtraBars.BarButtonItem
+		Private bar3 As DevExpress.XtraBars.Bar
+		Private barDockControlTop As DevExpress.XtraBars.BarDockControl
+		Private barDockControlBottom As DevExpress.XtraBars.BarDockControl
+		Private barDockControlLeft As DevExpress.XtraBars.BarDockControl
+		Private barDockControlRight As DevExpress.XtraBars.BarDockControl
+		Private imageCollection1 As DevExpress.Utils.ImageCollection
+	End Class
+End Namespace
+
